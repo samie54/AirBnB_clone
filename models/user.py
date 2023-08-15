11e95d686c8813760a8746010cc94ff106c2e8b9
samie54 <126026530+samie54@users.noreply.github.com>
2023-08-15 10:22:15 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-'''A class user that inherent from BaseModel'''
+"""Module for User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''represent a class User'''
-
+    """Class representing a User."""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
