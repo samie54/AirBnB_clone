70e24a81b129a143e5fd4bcb76ffa522d677dd20
samie54 <126026530+samie54@users.noreply.github.com>
2023-08-15 10:21:24 -0700

    review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-'''class inherent of BaseModel'''
+"""Module for Review class."""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    '''class Review'''
-
+    """Class representing a Review."""
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
-
