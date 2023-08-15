a6b96750f334708b745ca73d05f777cd18a6d88d
samie54 <126026530+samie54@users.noreply.github.com>
2023-08-15 10:16:30 -0700

    __init__.py

--- tests/test_models/__init__.py
+++ tests/test_models/__init__.py
@@ -1,8 +1,1 @@
-#!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-
-"""A variable storage, an instance of FileStorage"""
-storage = FileStorage()
-storage.reload()
 
